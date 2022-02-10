77573becea401045249970c8531c3a7920717692
basel alalami <alalamibasel@outlook.com>
2022-02-10 23:45:50 +0200

    share module

---
+++ src/app/shared/app-paginations/app-paginations.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppPaginationComponent } from './app-pagination/app-pagination.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppPaginationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppPaginationsModule { }
